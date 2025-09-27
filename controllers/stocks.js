bb4e634c6e624a75842a36dc47688abc0c982e9d
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-27 11:16:55 +0300

    new page for stock purchase

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -5,10 +5,11 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
     const user = await User.findById(req.params.userId).populate('stocks');
     res.render('stocks/index.ejs', {user});
-})
+});
 router.get('/new', async (req, res) => {
-    const user = await User.findById(req.params.userId);
-    res.render('stocks/new.ejs', {user});
+    res.render('stocks/new.ejs', {
+        user: req.session.user,
+    });
 });
 
 module.exports = router;
